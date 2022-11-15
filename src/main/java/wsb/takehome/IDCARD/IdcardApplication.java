be05bfc3d08b44afa6405e8c9dae0a9427a7e4b1
zakieki55 <zakueki@gmail.com>
2022-11-15 12:08:23 +0700

    Make a new Project named IDCARD

---
+++ src/main/java/wsb/takehome/IDCARD/IdcardApplication.java
@@ -0,0 +1,13 @@
+package wsb.takehome.IDCARD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wsb/takehome/IDCARD/IdcardApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.takehome.IDCARD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

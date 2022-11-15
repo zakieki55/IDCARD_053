10a333b60863039d90678f3d40a6fd74223e1370
zakieki55 <zakueki@gmail.com>
2022-11-15 12:51:31 +0700

    make myController to hold the value and pass it to main.html

--- src/main/java/wsb/takehome/IDCARD/myController.java
+++ src/main/java/wsb/takehome/IDCARD/myController.java
@@ -4,9 +4,17 @@
  */
 package wsb.takehome.IDCARD;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -16,8 +24,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class myController {
     @RequestMapping("/kirimData")
     @ResponseBody
-    public String getData(){
+    public String getData(@RequestParam("myName") String name,
+                          @RequestParam("myImage") MultipartFile image,
+                          @RequestParam("myaddress") String address,
+                          @RequestParam("myDate")
+                          @DateTimeFormat(pattern="yyyy-MM-dd") Date date,
+                          Model model) throws IOException{
+        String blob = Base64.encodeBase64String(image.getBytes());
+        String img ="data:image/png;base64,".concat(blob);
+        SimpleDateFormat tanggal = new SimpleDateFormat("dd-MMMM-yyyy");
+        String newTanggal = tanggal.format(date);
         
-        return "oke";
+        model.addAttribute("nama", name);
+        model.addAttribute("tanggal", newTanggal);
+        model.addAttribute("alamat", address);
+        model.addAttribute("foto", img);
+        
+        return"main";
     }
 }
